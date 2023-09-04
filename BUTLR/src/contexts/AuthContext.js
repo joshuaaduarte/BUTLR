a49a001b5c7e7bfb92fac8c49f79d55f6c128838
joshuaaduarte <jad6442@tamu.edu>
2023-09-03 19:22:50 -0500

    Scrapped old log in and log out. Created new sign up, sign in, forgot password,  dashboard, and update profile page that are functional and store user data through firebase.

---
+++ BUTLR/src/contexts/AuthContext.js
@@ -0,0 +1,64 @@
+import React, {useContext, useState, useEffect} from 'react'
+import { auth } from "../firebase"
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut,
+     sendPasswordResetEmail, updateEmail, updatePassword} from 'firebase/auth'
+
+const AuthContext = React.createContext()
+
+export function useAuth() { 
+    return useContext(AuthContext) 
+}
+
+export function AuthProvider({children}) {
+    const [currentUser, setCurrentUser] = useState()
+    const [loading, setLoading] = useState(true)
+
+  function signup(email, password) {
+    return createUserWithEmailAndPassword(auth, email, password)
+  }
+
+  function login(email, password) {
+    return signInWithEmailAndPassword(auth, email, password)
+  }
+
+  function logout() {
+    return signOut(auth)
+  }
+
+  function resetPassword(email) {
+    return sendPasswordResetEmail(auth, email)
+  }
+
+  function updatesEmail(email) {
+    return updateEmail(currentUser,email)
+  }
+
+  function updatesPassword(password) {
+    return updatePassword(currentUser,password)
+  }
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+        setCurrentUser(user)        
+        setLoading(false)
+
+      })
+      return unsubscribe
+  }, [])
+
+  
+  const value = {
+    currentUser,
+    signup,
+    login,
+    logout,
+    resetPassword, 
+    updatesEmail,
+    updatesPassword, 
+  }
+    return (
+    <AuthContext.Provider value={value}>
+        {!loading && children}
+    </AuthContext.Provider>
+)
+}
